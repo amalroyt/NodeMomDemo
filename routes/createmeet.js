58990adb05300aa4843eb186c4afd56e0e9f15aa
unknown <rutujas@rutujas-w8.cybage.com>
2017-02-22 15:29:53 +0530

    Post & Delete Backend

--- routes/createmeet.js
+++ routes/createmeet.js
@@ -1,25 +1,96 @@
 var fs = require('fs');
 var sequelize = require("./dbconfiguration").sequelize; //import sequelize database object
 
+exports.getTypes = function(req, res) {
+	var query_status = "SELECT * FROM domo_meeting_type";
+	sequelize.query(query_status, {
+        type: sequelize.QueryTypes.SELECT
+      }).then(function(status_rows) {
+        res.format({
+          json: function() {
+			
+            res.send(status_rows);
+			
+           }
+        });
+      }).error(function(error) {
+        console.log("Query Error: " + error);
+      });
+}
+
+exports.getAttendees = function(req, res) {
+	var query_attendees = "SELECT id,CONCAT_WS(' ',firstName,lastName) as firstlast FROM domo_users";
+	sequelize.query(query_attendees, {
+        type: sequelize.QueryTypes.SELECT
+      }).then(function(att_rows) {
+        res.format({
+          json: function() {
+			
+            res.send(att_rows);
+			
+            
+          }
+        });
+      }).error(function(error) {
+        console.log("Query Error: " + error);
+      });
+}
+exports.getFaci = function(req, res) {
+	var query_facilitator = "SELECT id,CONCAT_WS(' ',firstName,lastName) as firstlast FROM domo_users";
+	sequelize.query(query_facilitator, {
+        type: sequelize.QueryTypes.SELECT
+      }).then(function(faci_rows) {
+        res.format({
+          json: function() {
+			
+            res.send(faci_rows);
+			
+            
+          }
+        });
+      }).error(function(error) {
+        console.log("Query Error: " + error);
+      });
+}
+
+exports.getRec = function(req, res) {
+	var query_recorder = "SELECT id,CONCAT_WS(' ',firstName,lastName) as firstlast FROM domo_users";
+	sequelize.query(query_recorder, {
+        type: sequelize.QueryTypes.SELECT
+      }).then(function(rec_rows) {
+        res.format({
+          json: function() {
+			
+            res.send(rec_rows);
+			
+            
+          }
+        });
+      }).error(function(error) {
+        console.log("Query Error: " + error);
+      });
+}
+
 exports.postMeeting = function(req, res) {
-    var meeting = {
-      id: 1,
-      status: "Open",
-      type: "Sprint Meet",
-      title: "Weekly Sprint Status meeting",
-	  purpose: "Weekly Sprint Status meeting",
-	  facilitator: "Sandeep Nabar",
-	  recorder: "Durgesh Ahire",
-	  venue: "CT1-6-7080",
-	  date: '2017-02-15',
-      startTime: '03:30:00',
-	  endTime: '05:00:00',
-      agenda: "• General Engineering updates• JIRA User Stories Status",
-      attendees: "Josh Nelson, Vikas Jadhav, Sandhya Aghav"
+	
+	 var meeting = {
+      //id: req.body.meeting_id,
+      status: req.body.meeting_status,
+      type: req.body.meeting_type,
+      title: req.body.meeting_title,
+	  purpose: req.body.meeting_purpose,
+	  facilitator: req.body.meeting_facilitator,
+	  recorder: req.body.meeting_recorder,
+	  venue: req.body.meeting_venue,
+	  date: req.body.meeting_date,
+      startTime: req.body.meeting_starttime,
+	  endTime: req.body.meeting_endtime,
+      agenda: req.body.meeting_agenda,
+      attendees: req.body.meeting_attendees
     }
-    var query = "INSERT INTO meeting (id,status,type,title,purpose,facilitator,recorder,venue,date,startTime,endTime,agenda,attendees)";
+    var query = "INSERT INTO domo_meeting_master (meetingStatus,meetingType,meetingTitle,meetingPurpose,meetingFacilitator,meetingRecorder,meetingVenue,meetingDate,startTime,endTime,meetingAgenda,meetingAttendees,active)";
     query += "VALUES (";
-    query += meeting.id + ",";
+    //query += meeting.id + ",";
     query += " '" + meeting.status + "',";
     query += " '" + meeting.type + "',";
     query += " '" + meeting.title + "',";
@@ -31,8 +102,9 @@ exports.postMeeting = function(req, res) {
 	query += " '" + meeting.startTime + "',";
 	query += " '" + meeting.endTime + "',";
     query += " '" + meeting.agenda + "',";
-    query += " '" + meeting.attendees + "' )";
-    var queryID = "SELECT * FROM meeting";
+    query += " '" + meeting.attendees + "',";
+	query += " " + 1 + " )"; 
+    var queryID = "SELECT * FROM domo_meeting_master";
 
     sequelize.query(query, {
       type: sequelize.QueryTypes.INSERT
@@ -43,8 +115,9 @@ exports.postMeeting = function(req, res) {
       }).then(function(rows) {
         res.format({
           json: function() {
-            res.send(rows);
+            //res.send(rows);
             console.log("Inserted");
+			
           }
         });
       }).error(function(error) {
@@ -54,3 +127,29 @@ exports.postMeeting = function(req, res) {
       console.log("Query Error: " + error);
     });
   }
+exports.deleteMeet = function(req, res) {
+	
+  sequelize.query("DELETE FROM domo_meeting_master WHERE meetingId=" + req.params.id, {
+    type: sequelize.QueryTypes.DELETE
+  }).then(function() {
+    res.send(req.params);
+    console.log("Deleted")
+  }).error(function(error) {
+    console.log("Query Error: " + error);
+  });
+};
+
+exports.getMeeting = function(req, res) {
+  //Use query method to get the data from server
+  sequelize.query("UPDATE domo_meeting_master SET active = 0 WHERE meetingId = " + req.params.id, {
+    type: sequelize.QueryTypes.SELECT
+  }).then(function(results) {
+    res.format({
+      json: function() {
+        res.send(results);
+      }
+    });
+  }).error(function(error) {
+    console.log("Query Error: " + error);
+  });
+};
