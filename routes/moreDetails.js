dc60ee1329264e856cf17050fbf7994316d41f5f
Sanjivani <sanjivanig@cybage.com>
2017-04-05 16:07:48 +0530

    error page

--- routes/moreDetails.js
+++ routes/moreDetails.js
@@ -1,7 +1,7 @@
 var sequelize = require("./dbconfiguration").sequelize;
 exports.moreDetails = function(req, res) {
   var meetingId = req.params.meetingId;
-  sequelize.query(" select meetingId,meetingTitle,meetingPurpose,meetingAgenda,Date_FORMAT(meetingDate, '%d-%m-%Y') AS meetingDate,group_concat(' ',firstName,lastName) as meetingAttendees FROM domo_meeting_master as t1 LEFT JOIN domo_users as t2 ON find_in_set(t2.id, t1.meetingAttendees) WHERE t1.meetingId = '" + meetingId + "' group by t1.meetingId", {
+  sequelize.query(" select meetingId,meetingTitle,meetingPurpose,meetingAgenda,Date_FORMAT(meetingDate, '%d-%m-%Y') AS meetingDate,group_concat(' ',firstName, ' ', lastName) as meetingAttendees FROM domo_meeting_master as t1 LEFT JOIN domo_users as t2 ON find_in_set(t2.id, t1.meetingAttendees) WHERE t1.meetingId = '" + meetingId + "' group by t1.meetingId", {
     type: sequelize.QueryTypes.SELECT
   }).then(function(results) {
     res.format({
