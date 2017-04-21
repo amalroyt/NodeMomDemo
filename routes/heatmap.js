450dd4d9d5c8001db4de7085423792259d1b2075
Rutuja Saptarshi <rutujas@cybage.com>
2017-04-21 12:26:26 +0530

    Backend Updated Graph

---
+++ routes/heatmap.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var sequelize = require("./dbconfiguration").sequelize; //import sequelize database object
+var counts = [];
+ exports.generateheatmap = function(req, res) {
+
+    sequelize.query("SELECT meetingTitle, DATE_FORMAT(meetingDate, '%Y-%m-%d') as meetingDate,COUNT(*) as count FROM domo_meeting_master GROUP BY meetingDate", {
+      type: sequelize.QueryTypes.SELECT
+    }).then(function(results) {
+      res.format({
+        json: function() {
+
+
+          res.send(results);
+
+        }
+      });
+    }).error(function(error) {
+      console.log("Query Error: " + error);
+    });
+}
+ exports.showmoredetails = function(req, res) {
+   console.log(req.params.id);
+    sequelize.query("SELECT m.meetingId, m.meetingTitle, d.meetingType from domo_meeting_master m join domo_meeting_type d on (m.meetingType = d.id) WHERE m.meetingDate = '" + req.params.id +"'",{
+      type: sequelize.QueryTypes.SELECT
+    }).then(function(results) {
+      res.format({
+        json: function() {
+
+
+          res.send(results);
+
+        }
+      });
+    }).error(function(error) {
+      console.log("Query Error: " + error);
+    });
+
+}
