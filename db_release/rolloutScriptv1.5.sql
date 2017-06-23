c492bc14e2aa55acf131df1ea64929d449c9ed60
amalroyt <amalroy.t@gmail.com>
2017-06-23 15:40:54 +0530

    3pm 23June

--- db_release/rolloutScriptv1.5.sql
+++ db_release/rolloutScriptv1.5.sql
@@ -14,12 +14,13 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 --
--- Database structure for`domo_mom_db`
+-- Database structure for database `domo_mom_db`
 --
-DROP DATABASE IF EXISTS `domo_mom_db`;
-CREATE DATABASE `domo_mom_db`;
-USE domo_mom_db;
+CREATE DATABASE  IF NOT EXISTS `domo_mom_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `domo_mom_db`;
+
 --
 -- Table structure for table `domo_discussion_type`
 --
@@ -60,20 +61,27 @@ CREATE TABLE `domo_meeting_action` (
   `expectedCompletion` date DEFAULT NULL,
   `actualCompletion` date DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT '0',
   `createdBy` int(11) DEFAULT NULL,
   `createdDate` date DEFAULT NULL,
-  `active` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `domo_mom_fk_meetingId_idx` (`meetingId`),
   KEY `domo_mom_fk_responsible_idx` (`responsible`),
   KEY `domo_mom_fk_status_idx` (`status`),
-  KEY `domo_mom_fk_created_By_idx` (`createdBy`),
-  CONSTRAINT `domo_mom_fk_meeting_action_createdBy` FOREIGN KEY (`createdBy`) REFERENCES `domo_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `domo_mom_fk_meetinId` FOREIGN KEY (`meetingId`) REFERENCES `domo_meeting_master` (`meetingId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `domo_mom_fk_responsible` FOREIGN KEY (`responsible`) REFERENCES `domo_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `domo_mom_fk_status` FOREIGN KEY (`status`) REFERENCES `domo_meeting_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domo_meeting_action`
+--
+
+LOCK TABLES `domo_meeting_action` WRITE;
+/*!40000 ALTER TABLE `domo_meeting_action` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domo_meeting_action` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `domo_meeting_master`
@@ -87,7 +95,6 @@ CREATE TABLE `domo_meeting_master` (
   `meetingStatus` int(11) DEFAULT NULL,
   `meetingType` int(11) DEFAULT NULL,
   `meetingTitle` varchar(50) DEFAULT NULL,
-  `meetingPurpose` varchar(50) DEFAULT NULL,
   `meetingFacilitator` int(11) DEFAULT NULL,
   `meetingRecorder` int(11) DEFAULT NULL,
   `meetingVenue` varchar(40) DEFAULT NULL,
@@ -115,9 +122,17 @@ CREATE TABLE `domo_meeting_master` (
   CONSTRAINT `domo_mom_meeting_fk_recorder` FOREIGN KEY (`meetingRecorder`) REFERENCES `domo_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `domo_mom_meeting_fk_status` FOREIGN KEY (`meetingStatus`) REFERENCES `domo_meeting_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `domo_mom_meeting_fk_type` FOREIGN KEY (`meetingType`) REFERENCES `domo_meeting_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=198 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `domo_meeting_master`
+--
+
+LOCK TABLES `domo_meeting_master` WRITE;
+/*!40000 ALTER TABLE `domo_meeting_master` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domo_meeting_master` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `domo_meeting_points`
@@ -132,24 +147,31 @@ CREATE TABLE `domo_meeting_points` (
   `discussionBy` int(11) DEFAULT NULL,
   `discussionType` int(11) DEFAULT NULL,
   `discussion` varchar(200) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT '0',
   `decisionBy` int(11) DEFAULT NULL,
   `decision` varchar(200) DEFAULT NULL,
   `createdBy` int(11) DEFAULT NULL,
   `createdDate` date DEFAULT NULL,
-  `active` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `domo_mom_fk_meetinId_idx` (`meetingId`),
   KEY `domo_mom_fk_discussedBy_idx` (`discussionBy`),
   KEY `domo_mom_fk_discussionType_idx` (`discussionType`),
   KEY `domo_mom_fk_decisionBy_idx` (`decisionBy`),
-  KEY `domo_mom_fk_meeting_points_createdBy_idx` (`createdBy`),
   CONSTRAINT `domo_mom_fk_decisionBy` FOREIGN KEY (`decisionBy`) REFERENCES `domo_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `domo_mom_fk_discussedBy` FOREIGN KEY (`discussionBy`) REFERENCES `domo_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `domo_mom_fk_discussionType` FOREIGN KEY (`discussionType`) REFERENCES `domo_discussion_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `domo_mom_fk_meetingId` FOREIGN KEY (`meetingId`) REFERENCES `domo_meeting_master` (`meetingId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `domo_mom_fk_meeting_points_createdBy` FOREIGN KEY (`createdBy`) REFERENCES `domo_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  CONSTRAINT `domo_mom_fk_meetingId` FOREIGN KEY (`meetingId`) REFERENCES `domo_meeting_master` (`meetingId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domo_meeting_points`
+--
+
+LOCK TABLES `domo_meeting_points` WRITE;
+/*!40000 ALTER TABLE `domo_meeting_points` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domo_meeting_points` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `domo_meeting_status`
@@ -162,7 +184,7 @@ CREATE TABLE `domo_meeting_status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -171,7 +193,7 @@ CREATE TABLE `domo_meeting_status` (
 
 LOCK TABLES `domo_meeting_status` WRITE;
 /*!40000 ALTER TABLE `domo_meeting_status` DISABLE KEYS */;
-INSERT INTO `domo_meeting_status` VALUES (1,'open'),(2,'closed');
+INSERT INTO `domo_meeting_status` VALUES (1,'Open'),(2,'Closed'),(3,'In Progress'),(4,'On Hold');
 /*!40000 ALTER TABLE `domo_meeting_status` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -197,6 +219,37 @@ LOCK TABLES `domo_meeting_type` WRITE;
 /*!40000 ALTER TABLE `domo_meeting_type` DISABLE KEYS */;
 INSERT INTO `domo_meeting_type` VALUES (1,'Stand Up '),(2,'Client Call'),(3,'Sprint Meet'),(4,'Other');
 /*!40000 ALTER TABLE `domo_meeting_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `domo_tasklogs`
+--
+
+DROP TABLE IF EXISTS `domo_tasklogs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domo_tasklogs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task` varchar(10) DEFAULT NULL,
+  `onTable` varchar(20) DEFAULT NULL,
+  `meetingId` int(11) DEFAULT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
+  `updatedDate` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domo_mom_fk_tasklogs_updatedBy_idx` (`updatedBy`),
+  KEY `domo_mom_fk_tasklogs_meetingId_idx` (`meetingId`),
+  CONSTRAINT `domo_mom_fk_tasklogs_meetingId` FOREIGN KEY (`meetingId`) REFERENCES `domo_meeting_master` (`meetingId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `domo_mom_fk_tasklogs_updatedBy` FOREIGN KEY (`updatedBy`) REFERENCES `domo_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domo_tasklogs`
+--
+
+LOCK TABLES `domo_tasklogs` WRITE;
+/*!40000 ALTER TABLE `domo_tasklogs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domo_tasklogs` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -277,7 +330,7 @@ CREATE TABLE `domo_users` (
 
 LOCK TABLES `domo_users` WRITE;
 /*!40000 ALTER TABLE `domo_users` DISABLE KEYS */;
-INSERT INTO `domo_users` VALUES (1,17296,'sandeepnab','sandeepnab@cybage.com','Sandeep','Nabar','sandeep@12345',3,1),(2,12610,'vikasja','vikasja@cybage.com','Vikas','Jadhav','vikas@12345',1,1),(3,7601,'sajus','sajus@cybage.com','Saju','Sasidharan','saju@12345',1,1),(4,12389,'pravink','pravink@cybage.com','Pravin','Katta','pravin@12345',1,1),(5,14513,'ravikumard','ravikumard@cybage.com','Ravikumar','Dadhaniya','ravikumard@12345',1,1),(6,14170,'durgesha','durgesha@cybage.com','Durgesh','Ahire','durgesha@12345',1,1),(7,17005,'pranavki','pranavki@cybage.com','Pranav','Kishore','pranavki@12345',1,1),(8,15377,'sagarga','sagarga@cybage.com','Sagar','Gaikwad','sagarga@12345',1,1),(9,15106,'anjaliw','anjaliw@cybage.com','Anjali','Walke','anjaliw@12345',1,1),(10,8032,'rupeshr','rupeshr@cybage.com','Rupesh','Ramteke','rupeshr@12345',1,1),(11,12368,'prasanths','prasanths@cybage.com','Prasanth','Soman','prasanth@12345',1,0);
+INSERT INTO `domo_users` VALUES (1,17296,'sandeepnab','sandeepnab@cybage.com','Sandeep','Nabar','sandeepnab@12345',3,1),(2,12610,'vikasja','vikasja@cybage.com','Vikas','Jadhav','vikasja@12345',1,1),(3,7601,'sajus','sajus@cybage.com','Saju','Sasidharan','sajus@12345',1,1),(4,12389,'pravink','pravink@cybage.com','Pravin','Katta','pravink@12345',1,1),(5,14513,'ravikumard','ravikumard@cybage.com','Ravikumar','Dadhaniya','ravikumard@12345',1,1),(6,17005,'pranavki','pranavki@cybage.com','Pranav','Kishore','pranavki@12345',1,1),(7,15377,'sagarga','sagarga@cybage.com','Sagar','Gaikwad','sagarga@12345',1,1),(8,15106,'anjaliw','anjaliw@cybage.com','Anjali','Walke','anjaliw@12345',1,1),(9,8032,'rupeshr','rupeshr@cybage.com','Rupesh','Ramteke','rupeshr@12345',1,1),(10,8818,'pramodwa','pramodwa@cybage.com','Pramod','Wagale','pramodwa@12345',1,1);
 /*!40000 ALTER TABLE `domo_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -290,4 +343,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-22 17:55:42
+-- Dump completed on 2017-06-23 15:37:32
